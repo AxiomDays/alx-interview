523aefcde9ab74331825ea136a0844e79253047f
AxiomDays <tobeoke@gmail.com>
2024-10-01 13:09:08 +0100

    task0

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""
+a function for pascal's triangle
+"""
+
+def runTheMiddle(sumlist):
+    """ function that creates a list of the sum of the previous lists values, i.e the middle section of a pascal row """
+    templist2 = []
+    for k in range(len(sumlist)):
+        if k == (len(sumlist)-1):
+            break
+        print("funci is {}, and sumlist is {}, sum is {}".format(k, sumlist[k], sumlist[k]+sumlist[k+1]))
+        templist2.append(sumlist[k]+sumlist[k+1])
+
+    return templist2
+
+
+def pascal_triangle(n):
+    """ main func """
+    if n <= 0:
+        return []
+
+    mainlist = []
+
+    for i in range(n):
+        templist = []
+        tempcounter = 0
+        for j in range(i+1):
+            print("j is {} and i is {}".format(j, i))
+            if j == 0:
+                """ each row always starts with 1 """
+                templist.append(1)
+            elif j != (i) and j > 0:
+                """ only runs once, since it provides a list of the whole middle section """
+                if tempcounter == 0:
+                    templist.extend(runTheMiddle(mainlist[i-1]))
+                    tempcounter = 1
+            elif j == (i) and j > 0:
+                """ each row ends with 1 """
+                templist.append(1)
+            else:
+                continue
+        print("templist:{}".format(templist))
+        mainlist.append(templist)
+
+    print("{}".format(mainlist))
+    return mainlist
+
