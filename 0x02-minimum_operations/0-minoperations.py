5d05234aaccbb3136068745b9515036ab88e6385
AxiomDays <tobeoke@gmail.com>
2024-10-18 03:22:21 +0100

    min_op

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Script that computes a minimum operations """
+
+
+def minOperations(n):
+    """
+    func to compute the minimum number
+    """
+    if n < 2:
+        return 0
+    factor_list = []
+    i = 1
+    while n != 1:
+        i += 1
+        if n % i == 0:
+            while n % i == 0:
+                n /= i
+                factor_list.append(i)
+    return sum(factor_list)
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,1 @@
+ALX readme for minimum operations
