b36778a94eba8b06ea05ef3dffc87dbdf3ac775f
AxiomDays <tobeoke@gmail.com>
2024-12-03 23:22:53 +0100

    taskplease2

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+island perimeter main function
+"""
 
 def island_perimeter(grid):
+    """
+    funtion that checks the perimeter
+    of an island made of 1s
+    """
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
