fc365a707221f11d47fc701c6fe3d989f14d3875
AxiomDays <tobeoke@gmail.com>
2024-12-04 02:14:27 +0100

    phew

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,34 +9,19 @@ def island_perimeter(grid):
     of an island made of 1s
     """
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+
+    width = len(grid[0])
+    length = len(grid)
+
+    for i in range(length):
+        for j in range(width):
             if grid[i][j] == 1:
-                try:
-                    if grid[i][j-1] == 0:
-                        perimeter += 1
-                except IndexError:
+                if i == 0 or grid[i-1][j] == 0:
                     perimeter += 1
-                try:
-                    if grid[i][j+1] == 0:
-                        perimeter += 1
-                except IndexError:
+                if j == (width-1) or grid[i][j+1] == 0:
                     perimeter += 1
-                try:
-                    if grid[i-1][j] == 0:
-                        perimeter += 1
-                except IndexError:
+                if j == 0 or grid[i][j-1] == 0:
                     perimeter += 1
-                try:
-                    if grid[i+1][j] == 0:
-                        perimeter += 1
-                except IndexError:
-                    perimeter += 1
-                if i == 0:
-                    perimeter += 1
-                if j == 0:
+                if i == (length-1) or grid[i+1][j] == 0:
                     perimeter += 1
     return perimeter
-
-
-
